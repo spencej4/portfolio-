0b4dc0881768fd6e3cee1410017d69f34b78942b
User <user@Users-MacBook-Pro.local>
2019-04-24 09:01:06 -0700

    updates for style

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './navbar.css';
-import {nav} from '../../actions/index'
+// import {nav} from '../../actions/index'
 import {connect} from 'react-redux';
 import smoothScroll from 'smoothscroll'
 
