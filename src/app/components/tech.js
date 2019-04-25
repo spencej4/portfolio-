2a1f8ffe5d47b5e4e5aff56a16394b3ee3a3e452
User <user@Users-MacBook-Pro.local>
2019-04-24 20:43:20 -0700

    updates

--- src/app/components/tech.js
+++ src/app/components/tech.js
@@ -7,12 +7,12 @@ class Tech extends React.Component{
       <div id='tech'>
         <div className='tech-title'>Technologies</div>
         <div className="icon-container">
-          <div className="icon">MongoDB</div>
           <div className="icon">React</div>
           <div className="icon">NodeJS</div>
           <div className="icon">Express</div>
           <div className="icon">Javascript</div>
           <div className="icon">MySQL</div>
+          <div className="icon">MongoDB</div>
           <div className="icon">HTML</div>
           <div className="icon">CSS</div>
           <div className="icon">JQuery</div>
