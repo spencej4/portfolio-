c2d4f1940928df581ce714a345dfb35f3fe8fee2
User <user@Users-MacBook-Pro.local>
2019-04-25 17:03:32 -0700

    updates

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -11,12 +11,9 @@ class About extends React.Component {
                     <img className='spence' src='http://via.placeholder.com/250x250' alt='placeholder'></img>
                     <div className='about-text'>
                         <div className='about-title'><p>Nice to meet you.</p></div>
-                        <p>Lorem ipsum dolor amet banh mi seitan asymmetrical cred flannel. 
-                            Selfies locavore mlkshk, sustainable chicharrones selvage tilde. Selvage beard fashion axe, 
-                            adaptogen forage yr whatever wayfarers twee XOXO lo-fi air plant cray. Intelligentsia typewriter 
-                            street art beard direct trade farm-to-table la croix, copper mug offal retro. Organic stumptown 
-                            ugh, blue bottle la croix quinoa mumblecore. Squid godard biodiesel direct trade, humblebrag 
-                            chambray snackwave knausgaard activated charcoal</p>  
+                        <p>Forward thinking web developer with an eye for design and clean, minimalist UI's. I'm focused and engergized within fast paced environments.
+                            I enjoy building, interacting within diverse teams, and delivering on time products. With a background in maintaining airline hardware,
+                            complex aeronautics systems, troubleshooting and creating resolutions to problems. I develop thoughtful, efficient apps with the user experience in mind.</p>  
                         <a href='https://github.com/spencej4'  target="blank" className='link-inline'><img  id='github' src={github} alt='github link'></img></a>
                         <a href='https://www.linkedin.com/in/spencej4/'  target="blank" className='link-inline'><img  id='linkedin' src={linkedin} alt='linkedin link'></img></a>
                         <a href="link/to/your/download/file" className='link-inline' download>Resume Download</a>
