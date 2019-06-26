8188b1479af2c6f030f29e00cb4f87943d6e612b
User <user@Users-MacBook-Pro.local>
2019-06-26 09:20:31 -0700

    adds google analytics

--- src/app/app.js
+++ src/app/app.js
@@ -5,6 +5,15 @@ import Tech from './components/tech';
 import Work from './components/work';
 import About from './components/about';
 import Contact from './components/contact';
+import ReactGA from 'react-ga';
+
+// enables Google Analytics
+function initializeReactGA() {
+  ReactGA.initialize('UA-142763663-1');
+  ReactGA.pageview('/');
+}
+
+initializeReactGA();
 
 class App extends React.Component{
   render(){
